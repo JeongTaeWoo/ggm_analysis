ceb38d898f1cd9b44dc1ac30611dc14e3fd32ff3
JeongTaeWoo <tw010507@naver.com>
2025-07-16 23:04:22 +0900

    7.16

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ output_path_batch = base_dir / "적합 결과.xlsx"
 output_path_weight = base_dir / "가중치 측정 결과.xlsx"
 
 
-year, sex, Dx, Ex, age, observed_mu = func.load_excel(year = 2018, sex = "남자")
+year, sex, Dx, Ex, age, observed_mu = func.load_excel(year = 2011, sex = "여자")
 
 
 #--------------------
 #func.run_batch(years = range(1990, 2021), #전체 범위는 (1970, 2024)
-#               sex = '여자', df = df, trial = 200,
+#               sex = sex, df = df, trial = 200,
 #               center = 80, scale = 3, max_weight = 5,
 #               output_path = output_path_batch)
 #--------------------
@@ -42,7 +42,7 @@ func.fitting_gm(year = year, sex = sex, age = age)
 try:
     scale_result = func.get_scale_data_from_file(output_path_weight, year, sex)
     best_result, best_logL, best_scale_params = func.find_best_scale(year = year, sex = sex, trial = 100, 
-                        center_range = (85, 96, 1), scale_range = (1.0, 10.1, 0.5), max_weight_range = (2, 20, 1), n_runs = 30,
+                        center_range = 91, scale_range = (1.0, 10.1, 0.5), max_weight_range = (2, 20, 1), n_runs = 50,
                         Dx = Dx, Ex = Ex, age = age, 
                         best_logL = scale_result['logL'])
     func.save_scale_result_to_excel(best_result, best_logL, best_scale_params, year, sex, filepath = output_path_weight)
