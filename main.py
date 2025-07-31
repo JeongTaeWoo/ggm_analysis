43b6c304afa9bd64e43532077c4f345550680ef8
JeongTaeWoo <tw010507@naver.com>
2025-07-31 21:23:53 +0900

    7.31_2

--- main.py
+++ main.py
@@ -16,23 +16,24 @@ df = pd.read_excel(life_table_path, sheet_name = "Sheet1")
 output_path_result = base_dir / "측정 결과.xlsx"
 
 
-year, sex, Dx, Ex, age, observed_mu = func.load_life_table(year = 2017, sex = "남자")
+year, sex, Dx, Ex, age, observed_mu = func.load_life_table(year = 2004, sex = "여자")
 
 #--------------------
 # TODO evaluate_fit_metrics에 항목 추가? 어떤거?
 
 # TODO LAR 가지고도 뭔가 할수있을까?
 
-# TODO 논문 읽었던거 내용 간단하게라도 정리해서 모아두기
+# TODO 논문 읽었던거 내용 간단하게라도 정리해서 모아두기 - 진행중
 
 # TODO 엑셀 업데이트 많이 됐으니까 엑셀 읽어다가 그래프 그려주고 + metrics 보여주는 함수 만들기
 #--------------------
 # center_range = (85, 96, 1), scale_range = (1.0, 10.1, 0.5), max_weight_range = (2, 20, 1)
 # center = previous_result['center'], scale = previous_result['scale'], max_weight = previous_result['max_weight']
+
 try:
     previous_result = func.get_data_from_file(output_path_result, year, sex)
-    func.find_best_scale(year = year, sex = sex, trial = 1000, n_runs = 1,
-                        center_range = 91, scale_range = 4, max_weight_range = 7,
+    func.find_best_scale(year = year, sex = sex, trial = 500, n_runs = 2,
+                        center_range = 89, scale_range = 4, max_weight_range = 7,
                         Dx = Dx, Ex = Ex, age = age, filepath = output_path_result, notice = True,
                         best_logL_ggm = previous_result['logL_ggm'], best_logL_gm = previous_result['logL_gm'])
 
@@ -45,19 +46,19 @@ except Exception as e:
 
 finally: 
     pass
-    #os.system("shutdown /h")    
+#     #os.system("shutdown /h")    
 #--------------------
 
 #-------------------- for문 사용할 때
 #center_range = (85, 96, 1), scale_range = (1.0, 10.1, 0.5), max_weight_range = (6, 12, 1), n_runs = 20,
-# result_gm = func.fitting_gm(year = year, sex = sex, age = age, show_graph = False)
-# for i in range(91, 92) :
-#     for j in [round(x, 1) for x in np.arange(4, 6.1, 0.5)]:
-#         for k in range(6, 12, 1):
+
+# for i in range(87, 89) :
+#     for j in [round(x, 1) for x in np.arange(2, 5.1, 0.5)]:
+#         for k in range(5, 10, 1):
 #             try:
 #                 print(f"center = {i}, scale = {j}, max weight = {k}")
 #                 previous_result = func.get_data_from_file(output_path_result, year, sex) 
-#                 func.find_best_scale(year = year, sex = sex, trial = 300,  n_runs = 1, 
+#                 func.find_best_scale(year = year, sex = sex, trial = 200,  n_runs = 1, 
 #                                     center_range = i, scale_range = j, max_weight_range = k, show_graph = False,
 #                                     Dx = Dx, Ex = Ex, age = age, filepath = output_path_result, notice = False,
 #                                     best_logL_ggm = previous_result['logL_ggm'], best_logL_gm = previous_result['logL_gm'])
@@ -70,10 +71,6 @@ finally:
 #             finally: 
 #                 pass
 #os.system("shutdown /h")                 
-#--------------------
 
 #--------------------
-#result = func.result_maker(1.12E-05,	0.120908336,	0.207022451,	0.0272633321)
-#func.fitted_plot(result, mu_obs)
-#--------------------
-notification.notify(title="작업 완료", timeout=5)
+# notification.notify(title="작업 완료", timeout=5)
