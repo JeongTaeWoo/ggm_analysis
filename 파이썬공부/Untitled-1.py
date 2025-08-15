10b3a4b12553ad48c2fbe675a2d358cf9d8eb5bb
JeongTaeWoo <tw010507@naver.com>
2025-08-15 12:28:02 +0900

    8.15

--- 파이썬공부/Untitled-1.py
+++ 파이썬공부/Untitled-1.py
@@ -1,25 +1,24 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn import linear_model
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
+import tensorflow as tf 
 
+w = tf.Variable(4.0)
+b = tf.Variable(1.0)
 
-bream_length = [25.4, 26.3, 26.5, 29.0, 29.0, 29.7, 29.7, 30.0, 30.0, 30.7, 31.0, 31.0, 31.5, 32.0, 32.0, 32.0, 33.0, 33.0, 33.5, 33.5, 34.0, 34.0, 34.5, 35.0, 35.0, 35.0, 35.0, 36.0, 36.0, 37.0, 38.5, 38.5, 39.5, 41.0, 41.0] 
-bream_weight = [242.0, 290.0, 340.0, 363.0, 430.0, 450.0, 500.0, 390.0, 450.0, 500.0, 475.0, 500.0, 500.0, 340.0, 600.0, 600.0, 700.0, 700.0, 610.0, 650.0, 575.0, 685.0, 620.0, 680.0, 700.0, 725.0, 720.0, 714.0, 850.0, 1000.0, 920.0, 955.0, 925.0, 975.0, 950.0]
+@tf.function
+def hypothesis(x) :
+    return w*x + b
 
-smelt_length = [9.8, 10.5, 10.6, 11.0, 11.2, 11.3, 11.8, 11.8, 12.0, 12.2, 12.4, 13.0, 14.3, 15.0] 
-smelt_weight = [6.7, 7.5, 7.0, 9.7, 9.8, 8.7, 10.0, 9.9, 9.8, 12.2, 13.4, 12.2, 19.7, 19.9]
+def mse_loss(y_pred, y):
+    return tf.reduce_mean(tf.square(y_pred - y))
 
-length = bream_length + smelt_length
-weight = bream_weight + smelt_weight
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9] # 공부하는 시간
+y = [11, 22, 33, 44, 53, 66, 77, 87, 95] # 각 공부하는 시간에 맵핑되는 성적
+optimizer = tf.optimizers.SGD(0.01)
 
-fish_data = [[l, w] for l,w in zip(length, weight)]
-
-fish_target = [1] * 35 + [0] * 14
-
-kn = KNeighborsClassifier()
-kn.fit(fish_data, fish_target)
-print(kn.score(fish_data, fish_target))
+for i in range(301) :
+    with tf.GradientTape() as tape :
+        y_pred = hypothesis(x)
+        cost = mse_loss(y_pred, y)
+    gradients = tape.gradient(cost, [w,b])
+    optimizer.apply_gradients(zip(gradients, [w,b]))
+    if i % 10 == 0 :
+        print("epoch : {:3} | w의 값 : {:5.4f} | b의 값 : {:5.4} | cost : {:5.6f}".format(i, w.numpy(), b.numpy(), cost))    
